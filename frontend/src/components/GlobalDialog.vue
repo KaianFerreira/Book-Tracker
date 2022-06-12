9e308964dc715223262b0efdec17547e2af52880
Kaian <kaianfsantos@gmail.com>
2022-06-12 15:44:00 -0300

    feat: global dialog

---
+++ frontend/src/components/GlobalDialog.vue
@@ -0,0 +1,59 @@
+<template>
+  <v-dialog
+    persistent
+    v-model="globalDialog.open"
+    transition="dialog-bottom-transition"
+    max-width="600"
+  >
+  <v-card>
+    <v-toolbar color="warning">
+      {{ globalDialog.title }}
+    </v-toolbar>
+    <v-card-text class="mt-5">
+      <v-alert
+        :value="true"
+        color="error"
+        icon="fa-warning"
+        dark
+        class="mb-5"
+      >
+        <p>
+          {{ globalDialog.message }}
+        </p>
+        <p>
+          {{ globalDialog.overline }}
+        </p>
+      </v-alert>
+    </v-card-text>
+    <v-card-actions class="justify-end">
+      <v-btn
+        color="grey lighten-1"
+        dark
+        @click="globalDialog.cancel()"
+      >
+        Cancel
+      </v-btn>
+      <v-btn
+        color="primary"
+        dark
+        @click="globalDialog.submit()"
+      >
+        Submit
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+  </v-dialog>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+export default {
+  computed: {
+    ...mapState(['globalDialog'])
+  }
+}
+</script>
+
+<style>
+
+</style>
--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -5,12 +5,29 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    globalDialog: {
+      open: false,
+      title: 'Warning',
+      message: 'You are about to delete this book.',
+      overline: 'Are you sure?',
+      type: 'warning',
+      submit: function () {},
+      cancel: function () {}
+    }
   },
   getters: {
   },
   mutations: {
+    globalDialog (state, payload) { state.globalDialog = payload },
+    closeDialog (state) { state.globalDialog.open = false }
   },
   actions: {
+    closeDialog ({ commit }) {
+      commit('closeDialog')
+    },
+    openDialog ({ commit }, payload) {
+      commit('globalDialog', payload)
+    }
   },
   modules: {
   }
